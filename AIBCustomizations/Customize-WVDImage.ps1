6c73b18cee2b5647ac6981c0e87684b87d20f605
Esther Barthel <cognitionIT@users.noreply.github.com>
2020-11-29 12:30:53 +0100

    Demo ready!

--- AIBCustomizations/Customize-WVDImage.ps1
+++ AIBCustomizations/Customize-WVDImage.ps1
@@ -19,12 +19,12 @@ New-Item -Path 'C:\Install' -ItemType Directory -Force | Out-Null
 Invoke-WebRequest -Uri 'https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v7.9.1/npp.7.9.1.Installer.exe' -OutFile 'c:\Install\npp.7.9.1.Installer.exe'
 Invoke-Expression -Command 'c:\Install\npp.7.9.1.Installer.exe /S'
 
-# Wait
+# Wait for Notepad++ installer to finish
 Start-Sleep -Seconds 10
 
 # Install Acrobat Reader DC
 Invoke-WebRequest -Uri 'ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/2001320064/AcroRdrDC2001320064_en_US.exe' -OutFile 'c:\Install\AcroRdrDC2001320064_en_US.exe'
-Invoke-Expression -Command 'c:\Install\AcroRdrDC2001320064_en_US.exe /sAll /rs /rps /msi /norestart /quiet EULA_ACCEPT=YES'
+Start-Process -FilePath 'c:\Install\AcroRdrDC2001320064_en_US.exe' -ArgumentList '/sAll /rs /rps /msi /norestart /quiet EULA_ACCEPT=YES' -Wait
 
-# Wait
+# Wait for Adobe Installer to finish
 Start-Sleep -Seconds 10
