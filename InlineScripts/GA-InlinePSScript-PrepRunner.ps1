f4ef7104de4e65de37a4ce923d2e9b22b5708936
Esther Barthel <cognitionIT@users.noreply.github.com>
2021-03-16 22:10:25 +0100

    update runner

---
+++ InlineScripts/GA-InlinePSScript-PrepRunner.ps1
@@ -0,0 +1,28 @@
+          # Create the Bicep install folder
+          $installPath = "$env:USERPROFILE\.bicep"
+          $installDir = New-Item -ItemType Directory -Path $installPath -Force
+          $installDir.Attributes += 'Hidden'
+          # Fetch the latest Bicep CLI binary
+          (New-Object Net.WebClient).DownloadFile("https://github.com/Azure/bicep/releases/latest/download/bicep-win-x64.exe", "$installPath\bicep.exe")
+          # Add bicep to your PATH
+          $currentPath = (Get-Item -path "HKCU:\Environment" ).GetValue('Path', '', 'DoNotExpandEnvironmentNames')
+          if (-not $currentPath.Contains("%USERPROFILE%\.bicep")) { setx PATH ($currentPath + ";%USERPROFILE%\.bicep") }
+          if (-not $env:path.Contains($installPath)) { $env:path += ";$installPath" }
+          # Copy the bicep file to %systemroot%\system32 if it is not already there
+          if (-not(Test-Path "$($env:SystemRoot)\System32\bicep.exe"))
+          {
+            Copy-Item "$installPath\bicep.exe" -Destination "$($env:SystemRoot)\System32" -Force
+          }
+          # Verify you can now access the 'bicep' command.
+          bicep --help
+          # Done!
+          
+          ## Add Az PowerShell Module version 5.6.0 to the runner (if not already on the runner)
+          $minAzModuleVersion = '5.6.0'
+          if(!(Test-Path "C:\Modules\az_$minAzModuleVersion")) {
+            Install-Module -Name Az -AllowClobber -Scope CurrentUser -Force
+            Save-Module -Path "C:\Modules\az_$minAzModuleVersion" -Name Az -RequiredVersion $minAzModuleVersion -Force -ErrorAction Stop
+          }
+          $env:PSModulePath = "C:\Modules\az_$($minAzModuleVersion);$($env:PSModulePath)"
+          # Check installed versions of Az Module
+          Get-InstalledModule -Name Az -AllVersions | sort Version -Descending
--- InlineScripts/GA-bicep-InlinePSscript.ps1
+++ InlineScripts/GA-bicep-InlinePSscript.ps1
@@ -1,52 +1,40 @@
-﻿          ## Update the Az Module to 5.6.0+ (does not work with GitHub Actions runner as it is not installed using Install-Module)
-          #Update-Module -Name Az -RequiredVersion 5.6.0 -Force #-ErrorAction SilentlyContinue
-
-          # Read the GitHub Actions variables
+﻿          # Read the GitHub Actions variables
           [string]$githubWorkspace = "${{GITHUB.WORKSPACE}}"
 
-          # Read the environment variables
+          # Read the environment variables in PowerShell
           [string]$location = [System.Environment]::GetEnvironmentVariable('LOCATION')
           [string]$bicepFile = [System.Environment]::GetEnvironmentVariable('BICEP_FILE')
           [string]$resourcegroupName = [System.Environment]::GetEnvironmentVariable('RESOURCE_GROUP_NAME')
           
-          # Debug info:
           Write-Output ("* BICEP FILE: " + $($bicepFile))
           Write-Output ("* RESOURCE GROUP NAME: " + $($resourcegroupName))
           Write-Output ("* GITHUB_WORKSPACE: " + $($githubWorkspace))
 
-          # Use the resource group name as a postfix for the wvd components
           $namePostFix = $resourcegroupName.Replace("rg-","")
 
-          # Create a Template Parameter Object (hashtable)
+          ## Create a Template Parameter Object (hashtable)
           $objTemplateParameter = @{
             "location" = "$($location)";
-            "workSpaceName" = "ws-wvd-$($namePostFix)";
-            "hostpoolName" = "hp-wvd-$($namePostFix)";
-            "appgroupName" = "ag-wvd-$($namePostFix)";
+            "workSpaceName" = "ws-$($namePostFix)";
+            "hostpoolName" = "hp-$($namePostFix)";
+            "appgroupName" = "ag-$($namePostFix)";
             "preferredAppGroupType" = "Desktop";
             "hostPoolType" = "pooled";
             "loadbalancertype" = "DepthFirst";
             "appgroupType" = "Desktop";
           }
-          ## Show objTemplateParameter (debug info)
+          ## Show objTemplateParameter
           #$objTemplateParameter
           
           # Location of the bicep file in the local checked-out repo
           $biceptemplateFile = [string]("$($githubWorkspace)" + "\bicep\" + "$($bicepFile)")
-          # Debug info: 
           Write-Output ("* BICEP TEMPLATE FILE: " + $($biceptemplateFile))
 
-          # Create the resourcegroup
-          $newResourceGroupParams = @{
-            Name     = $resourcegroupName
-            Location = $location
+          # Create the resourceGroup (if it does not yet exist)
+          if (!(Get-AzResourceGroup -Name $resourcegroupName)) {
+            New-AzResourceGroup -Name $resourcegroupName -Location $location
           }
-          New-AzResourceGroup @newResourceGroupParams
 
-          # Deploy Azure resources based on bicep file as ARM Template file
-          $newResourceGroupDeploymentParams = @{
-            ResourceGroupName = $resourcegroupName
-            TemplateFile      = $($biceptemplateFile)
-            TemplateParameterObject = $objTemplateParameter
-          }
-          New-AzResourceGroupDeployment @newResourceGroupDeploymentParams -Verbose
+          # ARM Template file
+          ## Deploy resources based on bicep file for ARM Template
+          New-AzResourceGroupDeployment -ResourceGroupName $resourcegroupName -TemplateFile $($biceptemplateFile) -TemplateParameterObject $objTemplateParameter -Verbose
