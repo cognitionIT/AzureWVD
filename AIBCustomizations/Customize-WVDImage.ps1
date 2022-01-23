3331a66d477b1592926c302cb15a61ab0eb21f38
Cloud DevOps ninja <esther@cloud-devops.ninja>
2022-01-23 09:43:31 +0100

    new try

--- AIBCustomizations/Customize-WVDImage.ps1
+++ AIBCustomizations/Customize-WVDImage.ps1
@@ -34,44 +34,20 @@ Start-Sleep -Seconds 180
 #endregion
 
 #region Install Visual Studio Code
-# Download URL, you may need to update this if it changes
-$downloadUrl = "https://go.microsoft.com/fwlink/?LinkID=623230"
+Invoke-WebRequest -Uri 'https://go.microsoft.com/fwlink/?LinkID=623230' -OutFile 'c:\Install\vscode-install.exe'
+Invoke-Expression -Command "C:\Install\vscode-install.exe /silent /mergetasks='!runcode,addcontextmenufiles,addcontextmenufolders,associatewithfiles,addtopath'"
 
-# What to name the file and where to put it
-$installerFile = "vscode-install.exe"
-$installerPath = (Join-Path $env:TEMP $installerFile)
-
-# Install Options
-# Reference:
-# http://stackoverflow.com/questions/42582230/how-to-install-visual-studio-code-silently-without-auto-open-when-installation
-# http://www.jrsoftware.org/ishelp/
-# I'm using /silent, use /verysilent for no UI
-
-# Install with the context menu, file association, and add to path options (and don't run code after install: 
-$installerArguments = "/silent /mergetasks='!runcode,addcontextmenufiles,addcontextmenufolders,associatewithfiles,addtopath'"
-
-#Install with default options, and don't run code after install.
-#$installerArguments = "/silent /mergetasks='!runcode'"
-
-Write-Verbose "Downloading $installerFile..."
-Invoke-Webrequest $downloadUrl -UseBasicParsing -OutFile $installerPath
-
-Write-Verbose "Installing $installerPath..."
-Start-Process $installerPath -ArgumentList $installerArguments -Wait
-
-Write-Verbose "Cleanup the downloaded file."
-Remove-Item $installerPath -Force
 # Wait for Visual Studio Code installer to finish
-Start-Sleep -Seconds 10
+Start-Sleep -Seconds 180
 #endregion
 
 #region Install Bicep vscode extension
 # Install the .NET Install Tool for Extension Authors dependecy for Bicep extension
-code --install-extension ms-dotnettools.vscode-dotnet-runtime --force
-Start-Sleep -Seconds 10
+Invoke-Expression -Command 'code --install-extension ms-dotnettools.vscode-dotnet-runtime --force'
+Start-Sleep -Seconds 180
 # Install the Bicep extension
-code --install-extension ms-azuretools.vscode-bicep --force
-Start-Sleep -Seconds 10
+Invoke-Expression -Command 'code --install-extension ms-azuretools.vscode-bicep --force'
+Start-Sleep -Seconds 180
 #endregion
 
 #region Install PowerShell Az module
